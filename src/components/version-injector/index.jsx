52937c4d2fab9e3a8294603cfa0a4a5cd1c197d5
Mark Kurtz <mark.kurtz@neuralmagic.com>
2024-02-22 10:03:47 -0500

    fixes for install commands

--- src/components/version-injector/index.jsx
+++ src/components/version-injector/index.jsx
@@ -1,43 +1,58 @@
 import React from 'react';
 import {useDoc} from '@docusaurus/theme-common/internal';
 
-const VersionInjector = ({ children, targetVersion = 'VERSION', targetProduct = 'PRODUCT', prepend = '', currentTag = '', ignoreNightly = false}) => {
-    const { metadata } = useDoc();
-    const currentVersion = metadata.version;
 
-    const processChildren = (child) => {
-        if (typeof child === 'string') {
-            let version = currentVersion;
-            let product = targetProduct;
+const processChildren = (child, currentVersion, targetVersion, targetProducts, prepend, currentTag, ignoreNightly) => {
+    if (typeof child === 'string') {
+        let version = currentVersion;
+        let products = [...targetProducts];
 
-            if (version === 'current') {
-                if (!ignoreNightly) {
+        if (version === 'current') {
+            if (!ignoreNightly && targetProducts) {
+                for (let i = 0; i < products.length; i++) {
+                    let product = products[i];
                     if (product.indexOf("[") > -1) {
                         const split = product.split('[');
                         product = `${split[0]}-nightly[${split[1]}`;
                     } else {
                         product = `${product}-nightly`;
                     }
+                    products[i] = product;
                 }
-                version = currentTag;
-            } else if (prepend) {
-                version = `${prepend}${version}`;
             }
-
-            let replaced = child.replace(new RegExp(targetVersion, 'g'), version);
-            replaced = replaced.replace(new RegExp(targetProduct, 'g'), product);
-
-            return replaced;
-        } else if (React.isValidElement(child)) {
-            return React.cloneElement(child, child.props, processChildren(child.props.children));
-        } else if (Array.isArray(child)) {
-            return child.map(c => processChildren(c));
-        } else {
-            return child;
+            version = currentTag;
+        } else if (prepend) {
+            version = `${prepend}${version}`;
         }
-    };
 
-    return <>{processChildren(children)}</>;
+        let replaced = child.replace(new RegExp(targetVersion, 'g'), version);
+
+        for (let i = 0; i < products.length; i++) {
+            replaced = replaced.replace(new RegExp(targetProducts[i], 'g'), products[i]);
+        }
+
+        return replaced;
+    }
+
+    if (React.isValidElement(child)) {
+        return React.cloneElement(child, child.props, processChildren(child.props.children, currentVersion, targetVersion, targetProducts, prepend, currentTag, ignoreNightly));
+    }
+
+    if (Array.isArray(child)) {
+        return child.map(c => processChildren(c, currentVersion, targetVersion, targetProducts, prepend, currentTag, ignoreNightly));
+    }
+
+    return child;
+};
+
+
+const VersionInjector = ({ children, targetVersion = 'VERSION', targetProduct = 'PRODUCT', prepend = '', currentTag = '', ignoreNightly = false}) => {
+    const { metadata } = useDoc();
+    const currentVersion = metadata.version;
+    const targetProducts = Array.isArray(targetProduct) ? targetProduct : [targetProduct];
+    const processedChildren = processChildren(children, currentVersion, targetVersion, targetProducts, prepend, currentTag, ignoreNightly);
+
+    return <>{processedChildren}</>;
 };
 
 export default VersionInjector;
